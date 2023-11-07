c37484a89f3d901460e1cda9ad149efea4a63388
leonardoruizdiaz <ruizdiazleonardo2023@gmail.com>
2023-11-07 17:16:28 -0300

    backend

---
+++ index.js
@@ -0,0 +1,34 @@
+class ProductManager {
+    product = [];
+    constructor(){}
+    getProduct(){
+        console.log(this.product);
+    }
+    addProduct(id,title,description,price,thumbnail,code,stock){
+        id = 0;
+        for (let i = 0; i < this.product.length; i++) {
+            const element = this.product[i];
+            if(element.id > id){
+                id = element.id;
+            }    
+        }
+        id++;
+
+        code = 1;
+        for (let e = 0; e < this.product.length; e++) {
+            const codigo = this.product[e];
+            if(codigo.code > code){
+                code = codigo.code;
+            }    
+        }
+        code++;
+
+        this.product.push({id,title,description,price,thumbnail,code,stock})
+        
+    }
+    }
+    const producto = new ProductManager();
+    producto.addProduct(``,`cebolla`,`chica`,50,`imagencebolla`,``,10);
+    producto.addProduct(``,`calabaza`,`grande`,80,`imagencalabaza`,``,13);
+    producto.addProduct(``,`papa`,`blanca`,60,`imagenpapa`,``,40); 
+    producto.getProduct();
