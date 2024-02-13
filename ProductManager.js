2b72983ed0cd3cca79ffb24efcf884885df366cd
leonardoruizdiaz <ruizdiazleonardo2023@gmail.com>
2024-02-13 14:27:36 -0300

    cambios

--- ProductManager.js
+++ ProductManager.js
@@ -5,7 +5,8 @@ class ProductManager {
     product = [];
     constructor(){}
     getProduct(){
-        console.log(this.product);
+        
+        console.log();
     }
     addProduct(id,title,description,price,thumbnail,code,stock){
         id = 0;
@@ -25,13 +26,28 @@ class ProductManager {
             }    
         }
         code++;
-
         this.product.push({id,title,description,price,thumbnail,code,stock})
         
     }
     }
+
+    const fs = require("fs")
+
+
     const producto = new ProductManager();
     producto.addProduct(``,`cebolla`,`chica`,50,`imagencebolla`,``,10);
     producto.addProduct(``,`calabaza`,`grande`,80,`imagencalabaza`,``,13);
     producto.addProduct(``,`papa`,`blanca`,60,`imagenpapa`,``,40); 
+    producto.addProduct(``,`manzana`,`roja`,30,`imagenmanzana`,``,100);
     producto.getProduct();
+
+    let rutaProducto = "./Productos/producto.json"
+
+    fs.writeFileSync(rutaProducto, JSON.stringify(producto, null, 4))
+
+    let mostrarProductos = JSON.parse(fs.readFileSync(rutaProducto, {encoding:"utf-8"}))
+
+    console.log(mostrarProductos);
+    
+
+
